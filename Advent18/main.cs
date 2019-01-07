58edcd21aede1fb79370a0a00901cc1694f18686
elt12ddo <elt12ddo@student.lu.se>
2019-01-07 18:10:54 +0100

    Day 1 part 1, with better structure

---
+++ Advent18/main.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Day1{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day1Part1 p1 = new Day1Part1();
+            p1.findValue();
+
+        }
+    }
+}
